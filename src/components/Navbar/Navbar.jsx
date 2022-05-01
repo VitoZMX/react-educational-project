dec8e3ea4013feebeabfccc964f3ced2c839aa79
Виктор Жавнеровский <vi-69@yandex.ru>
2022-05-01 22:57:05 +0300

    20

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to='/profile'>Profile</NavLink>
+            <div className={s.item}>
+                <NavLink to='/profile' className={({isActive}) => ( isActive ? `${s.ActiveLink}` : 'inactiv')}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/dialogs">Massages</NavLink>
+                <NavLink to="/dialogs" className={({isActive}) => ( isActive ? `${s.ActiveLink}` : 'inactiv')}>Massages</NavLink>
             </div>
             <div className={s.item}><a>News</a></div>
             <div className={s.item}><a>Music</a></div>
