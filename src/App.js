1dc46d5dab30edb626fffc03d83785685fe9f67f
Виктор Жавнеровский <vi-69@yandex.ru>
2022-05-01 17:55:32 +0300

    12

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from "react"
 import './App.css'
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import Profile from "./components/Profile"
 
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src="https://png.pngtree.com/element_our/png/20180912/coffee-time-png_91570.jpg"/>
-            </header>
-            <nav className='nav'>
-                <div><a>Profile</a></div>
-                <div><a>Massages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
-                </div>
-                <div>ava + description</div>
-                <div>My posts
-                    <div>New post</div>
-                    <div>
-                        <div>post 1</div>
-                        <div>post 2</div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     )
 }
