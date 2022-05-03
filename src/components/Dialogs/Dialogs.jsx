ad1b950cbac640e890aca6442234c7c0786bd803
Виктор Жавнеровский <vi-69@yandex.ru>
2022-05-03 18:31:30 +0300

    21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,23 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Vito
+                </div>
+                <div className={s.dialog}>
+                    Max
+                </div>
+                <div className={s.dialog}>
+                    Li
+                </div>
+            </div>
+            <div className={s.massages}>
+                <div className={s.massage}>Hi</div>
+                <div className={s.massage}>How are u?</div>
+                <div className={s.massage}>Yo</div>
+            </div>
         </div>
     )
 }
