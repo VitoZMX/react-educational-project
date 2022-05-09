8849144b09ec6b83d88ae5a14387ba07616f3771
Виктор Жавнеровский <vi-69@yandex.ru>
2022-05-09 19:14:01 +0300

    31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,14 +4,22 @@ import Post from './Post/Post'
 
 const MyPosts = (props) => {
     let postElements = props.posts.map(p => <Post message={p.message} likeCoun={p.likesCount}/>)
+    let newPostElement = React.createRef()
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
 
     return (
         <div className={s.conteinerPosts}>
             <h3>My posts</h3>
             <div>
-                <div><textarea></textarea></div>
                 <div>
-                    <button>Add post</button>
+                    <textarea ref={newPostElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
                     <button>Remove</button>
                 </div>
             </div>
