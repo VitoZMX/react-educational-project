ff13b17f62824ee46de015b8bd039f032173b5ba
Виктор Жавнеровский <vi-69@yandex.ru>
2022-05-11 18:20:02 +0300

    35

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import reportWebVitals from './reportWebVitals'
 import './index.css'
-import {rerenderEntireTree} from './render'
-import state from './redux/state'
+import state, {addPost, updateNewPostText, subscribe} from './redux/state'
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import App from './App'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+let rerenderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            </BrowserRouter>
+        </React.StrictMode>
+    )
+}
 
 rerenderEntireTree(state)
-
+subscribe(rerenderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from '../render'
+let rerenderEntireTree = () => {
+    console.log('just text')
+}
 
 let state = {
     profilePage: {
@@ -28,7 +30,7 @@ let state = {
     sidebar: {}
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -40,9 +42,13 @@ export let addPost = () => {
     rerenderEntireTree(state)
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText
     rerenderEntireTree(state)
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer
 }
 
 export default state
