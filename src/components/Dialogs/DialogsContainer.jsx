5c03a20e3153d5d8670d229ae8d196ec885501a8
Виктор Жавнеровский <vi-69@yandex.ru>
2022-05-18 17:51:26 +0300

    45

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 import {sendMessageCreator, updateNewMessageBodyTextCreator} from '../../redux/dialogs-reducer'
 import Dialogs from './Dialogs'
-import StoreContext from '../../storeContext'
+import {connect} from 'react-redux'
 
-const DialogsContainer = () => {
-
-    return <StoreContext.Consumer>
-        {(store) => {
-            let state = store.getState().dialogsPage
-
-            let onSendMessageClick = () => {
-                store.dispatch(sendMessageCreator())
-            }
-
-            let onNewMassageChange = (body) => {
-                store.dispatch(updateNewMessageBodyTextCreator(body))
-            }
-
-            return <Dialogs updateNewMessageBodyText={onNewMassageChange} sendMessage={onSendMessageClick}
-                            dialogsPage={state}/>
-        }
-        }
-    </StoreContext.Consumer>
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
 }
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBodyText: () => {
+            dispatch(sendMessageCreator())
+        },
+        sendMessage: (body) => {
+            dispatch(updateNewMessageBodyTextCreator(body))
+        }
+    }
+}
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
 
 export default DialogsContainer
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import ReactDOM from 'react-dom/client'
-import {Provider} from './storeContext'
+import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
     root.render(
         <React.StrictMode>
             <BrowserRouter>
