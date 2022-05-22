80afedb0b3216fd9d5576a93d47a05e42d24da78
Виктор Жавнеровский <vi-69@yandex.ru>
2022-05-22 15:39:16 +0300

    54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,13 @@ import userPhoto from '../../assets/images/noimg.jpeg'
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props)
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            })
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                })
+        }
     }
 
     render() {
