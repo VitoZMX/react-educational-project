a8f64f58d00464e60e9d2828e9fdf328dc1c990b
VitoZMX <Vi-69@yandex.ru>
2022-10-02 14:05:40 +0300

    74

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -26,7 +26,14 @@ class ProfileStatus extends React.Component {
         })
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({status: this.props.status})
+        }
+    }
+
     render() {
+        console.log('render')
         return (
             <div>
                 {!this.state.editMode &&
