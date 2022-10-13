18d1a44b8946769e1bf1ea6e83b74528e66804ab
VitoZMX <Vi-69@yandex.ru>
2022-10-13 18:21:44 +0300

    78

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,27 +2,19 @@ import React from 'react'
 import {Field, reduxForm} from 'redux-form'
 import {Input} from '../common/FormsControls/FormsControls'
 import {required} from '../../utils/validators/validators'
-
-const Login = (props) => {
-
-    const onSubmit = (formData) => {
-        console.log(formData)
-    }
-
-    return <div>
-        <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit}/>
-    </div>
-}
+import {connect} from 'react-redux'
+import {login} from '../../redux/auth-reducer'
+import {Navigate} from 'react-router-dom'
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={'Login'} validate={[required]} name={'login'} component={Input}/>
+                <Field placeholder={'Email'} validate={[required]} name={'email'} component={Input}/>
             </div>
             <div>
-                <Field placeholder={'Password'} validate={[required]} name={'password'} component={Input}/>
+                <Field placeholder={'Password'} type={'password'} validate={[required]} name={'password'}
+                       component={Input}/>
             </div>
             <div>
                 <Field component={Input} name={'rememberMe'} type={'checkbox'}/> remember me
@@ -36,4 +28,25 @@ const LoginForm = (props) => {
 
 const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
-export default Login
+const Login = (props) => {
+
+    const onSubmit = (formData) => {
+        console.log(formData)
+        props.login(formData.email, formData.password, formData.rememberMe)
+    }
+
+    if (props.isAuth) {
+        return <Navigate to={'/profile'}/>
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+}
+
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+})
+
+export default connect(mapStateToProps, {login})(Login)
