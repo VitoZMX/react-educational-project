c49c51d4da0d98a838dac15aa60e16f1bd735a07
VitoZMX <Vi-69@yandex.ru>
2022-11-22 19:07:30 +0300

    93

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -5,34 +5,40 @@ import {NavLink} from 'react-router-dom'
 
 let User = ({user, followingInProgress, unfollow, follow}) => {
     return (
-        <div>
-                <span>
-                    <div>
-                        <NavLink to={'/profile/' + user.id}>
+        <div className={s.userMiniProfile}>
+            <div>
+                <div>
+                    <NavLink to={'/profile/' + user.id}>
                         <img src={user.photos.small != null ? user.photos.small : userPhoto} className={s.userPhoto}
                              alt='avaImg'/>
-                        </NavLink>
-                    </div>
-                    <div>
-                        {user.followed ?
-                            <button disabled={followingInProgress
-                                .some(id => id === user.id)}
-                                    onClick={() => {unfollow(user.id)}}>
-                                Unfollow</button>
-                            : <button disabled={followingInProgress
-                                .some(id => id === user.id)}
-                                      onClick={() => {follow(user.id)}}>
-                                Follow</button>}
-                    </div>
-                </span>
-            <span>
-                    <span>
-                        <div className={s.fullName}>{user.name}</div><div>{user.status}</div>
-                    </span>
-                    <span>
-                        <div>{'user1.location.country'}</div><div>{'user1.location.city'}</div>
-                    </span>
-                </span>
+                    </NavLink>
+                </div>
+                <div>
+                    {user.followed ?
+                        <button disabled={followingInProgress
+                            .some(id => id === user.id)}
+                                onClick={() => {
+                                    unfollow(user.id)
+                                }}>
+                            Unfollow</button>
+                        : <button disabled={followingInProgress
+                            .some(id => id === user.id)}
+                                  onClick={() => {
+                                      follow(user.id)
+                                  }}>
+                            Follow</button>}
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div className={s.fullName}>{user.name}</div>
+                    <div>{user.status}</div>
+                </div>
+                <div>
+                    <div>{'user.location.country'}</div>
+                    <div>{'user.location.city'}</div>
+                </div>
+            </div>
         </div>)
 }
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Paginator from '../common/Paginator/Paginator'
 import User from './User'
-
+import style from './Users.module.css'
 
 let Users = ({currentPage, onPageChanged, totalUserCounter, pageSize, users, ...props}) => {
     return <div>
         <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
-                   totalUserCounter={totalUserCounter} pageSize={pageSize}/>
-        <div>
+                   totalItemsCount={totalUserCounter} pageSize={pageSize}/>
+        <div className={style.usersContainer}>
             {
                 users.map(u => <User user={u}
                                      followingInProgress={props.followingInProgress}
